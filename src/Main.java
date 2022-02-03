a00ae50db24bcfa8ecd1713c3dcedc2247c07c19
WarPromo <kooltyme1@gmail.com>
2022-02-03 01:53:55 -0800

    autoopen image option (also read discord if u haven't already)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.awt.Desktop;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +13,7 @@ public class Main {
         identify("inputs/input1.jpeg", "outputs/output1.png");
         //identify("inputs/input2.jpeg", "outputs/output2.png");
         //identify("inputs/input3.png", "outputs/output3.png");
-        identify("inputs/input4.jpeg", "outputs/output4.png");
+        identify("inputs/input4.jpeg", "outputs/output4.png", true);
         //identify("inputs/input5.jpeg", "outputs/output5.png");
 
         System.out.println("finished with 0 errors!");
@@ -30,5 +33,24 @@ public class Main {
 
         output.setGrid(input.convert(newgrid));
 
+    }
+    private static void identify(String inputPath, String outputPath, boolean open) {
+        identify(inputPath, outputPath);
+        if(open){
+            if(!Desktop.isDesktopSupported()){
+                System.out.println("can't open file, desktop aint supported");
+            }
+            File file = new File(outputPath);
+            Desktop desktop = Desktop.getDesktop();
+
+            if(file.exists()) {
+                try {
+                    desktop.open(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else System.out.println("file don't exist");
+        }
     }
 }
